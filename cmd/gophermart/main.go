5c3c572e8bf54334a7073045c5e37580aec86644
Nick Klius <nicklls26@gmail.com>
2022-07-09 16:22:07 +0300

    IN5: fix bug #1

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
-	"go.uber.org/zap"
-
-	"github.com/nickklius/go-loyalty/config"
-	"github.com/nickklius/go-loyalty/internal/app"
+	"log"
+	"net/http"
 )
 
+type Handler struct{}
+
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	data := []byte("Hello World!")
+	w.Write(data)
+}
+
 func main() {
-	logger, _ := zap.NewProduction()
-	defer logger.Sync()
+	//logger, _ := zap.NewProduction()
+	//defer logger.Sync()
+	//
+	//cfg, err := config.NewConfig()
+	//if err != nil {
+	//	logger.Error(err.Error())
+	//}
 
-	cfg, err := config.NewConfig()
-	if err != nil {
-		logger.Error(err.Error())
-	}
+	log.Print("starting app, create handler")
+	handler := Handler{}
+	log.Print("run listener")
+	http.ListenAndServe(":9000", handler)
 
-	app.Run(cfg, logger)
+	//app.Run(cfg, logger)
+
 }
