014f55cb1e057b932736cfe4471c4a9f12dd43df
Nick Klius <nicklls26@gmail.com>
2022-07-09 19:51:16 +0300

    IN5: fix bug #29

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,19 +1,11 @@
 package app
 
 import (
-	"os"
-	"os/signal"
-	"syscall"
+	"net/http"
 
 	"go.uber.org/zap"
 
-	"github.com/go-chi/chi/v5"
 	"github.com/nickklius/go-loyalty/config"
-	"github.com/nickklius/go-loyalty/internal/handler"
-	"github.com/nickklius/go-loyalty/internal/httpserver"
-	"github.com/nickklius/go-loyalty/internal/storage/postgres"
-	"github.com/nickklius/go-loyalty/internal/usecase"
-	"github.com/nickklius/go-loyalty/internal/usecase/repo"
 )
 
 type App struct {
@@ -23,33 +15,36 @@ type App struct {
 func Run(cfg *config.Config, logger *zap.Logger) {
 	Migrate(cfg.DB.DatabaseURI)
 
-	pg, err := postgres.New(cfg.DB.DatabaseURI)
-	if err != nil {
-		logger.Error(err.Error())
-	}
-	defer pg.Close()
+	srv := &http.Server{Handler: nil, Addr: cfg.App.RunAddress}
+	srv.ListenAndServe()
 
-	useCases := usecase.New(
-		repo.New(pg),
-	)
-
-	h := chi.NewRouter()
-	handler.NewRouter(h, logger, useCases, cfg)
-
-	httpServer := httpserver.New(h, httpserver.Port(cfg.App.RunAddress))
-
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
-
-	select {
+	//pg, err := postgres.New(cfg.DB.DatabaseURI)
+	//if err != nil {
+	//	logger.Error(err.Error())
+	//}
+	//defer pg.Close()
+	//
+	//useCases := usecase.New(
+	//	repo.New(pg),
+	//)
+	//
+	//h := chi.NewRouter()
+	//handler.NewRouter(h, logger, useCases, cfg)
+	//
+	//httpServer := httpserver.New(h, httpserver.Port(cfg.App.RunAddress))
+	//
+	//interrupt := make(chan os.Signal, 1)
+	//signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+	//
+	//select {
 	//case s := <-interrupt:
 	//	logger.Info("app - Run - signal: " + s.String())
-	case err = <-httpServer.Notify():
-		logger.Error("app - Run - httpServer.Notify: " + err.Error())
-	}
-
-	err = httpServer.Shutdown()
-	if err != nil {
-		logger.Error("app - Run - httpServer.Shutdown: " + err.Error())
-	}
+	//case err = <-httpServer.Notify():
+	//	logger.Error("app - Run - httpServer.Notify: " + err.Error())
+	//}
+	//
+	//err = httpServer.Shutdown()
+	//if err != nil {
+	//	logger.Error("app - Run - httpServer.Shutdown: " + err.Error())
+	//}
 }
