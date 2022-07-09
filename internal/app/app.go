9f9a5faa83f7e4d8cfe31c6416ce7153fc664801
Nick Klius <nicklls26@gmail.com>
2022-07-09 16:14:59 +0300

    IN5: fix run in autotest

--- internal/app/app.go
+++ internal/app/app.go
@@ -9,11 +9,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/nickklius/go-loyalty/config"
-	"github.com/nickklius/go-loyalty/internal/handler"
 	"github.com/nickklius/go-loyalty/internal/httpserver"
-	"github.com/nickklius/go-loyalty/internal/storage/postgres"
-	"github.com/nickklius/go-loyalty/internal/usecase"
-	"github.com/nickklius/go-loyalty/internal/usecase/repo"
 )
 
 type App struct {
@@ -21,18 +17,18 @@ type App struct {
 }
 
 func Run(cfg *config.Config, logger *zap.Logger) {
-	pg, err := postgres.New(cfg.DB.DatabaseURI)
-	if err != nil {
-		logger.Error(err.Error())
-	}
-	defer pg.Close()
+	//pg, err := postgres.New(cfg.DB.DatabaseURI)
+	//if err != nil {
+	//	logger.Error(err.Error())
+	//}
+	//defer pg.Close()
 
-	useCases := usecase.New(
-		repo.New(pg),
-	)
+	//useCases := usecase.New(
+	//	repo.New(pg),
+	//)
 
 	h := chi.NewRouter()
-	handler.NewRouter(h, logger, useCases, cfg)
+	//handler.NewRouter(h, logger, useCases, cfg)
 	httpServer := httpserver.New(h, httpserver.Port(cfg.App.RunAddress))
 
 	interrupt := make(chan os.Signal, 1)
@@ -41,11 +37,11 @@ func Run(cfg *config.Config, logger *zap.Logger) {
 	select {
 	case s := <-interrupt:
 		logger.Info("app - Run - signal: " + s.String())
-	case err = <-httpServer.Notify():
+	case err := <-httpServer.Notify():
 		logger.Error("app - Run - httpServer.Notify: " + err.Error())
 	}
 
-	err = httpServer.Shutdown()
+	err := httpServer.Shutdown()
 	if err != nil {
 		logger.Error("app - Run - httpServer.Shutdown: " + err.Error())
 	}
